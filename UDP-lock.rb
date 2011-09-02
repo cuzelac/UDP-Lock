8c7abf093dc381aa82eb37f1e8270fb00967b80d
Chris Uzelac <cu@squareup.com>
2011-09-02 14:27:27 -0700

    class-ify UDPLock

--- UDP-lock.rb
+++ UDP-lock.rb
@@ -1,16 +1,35 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
 require 'socket'
 
-# test the basic case
-# enter the port you want to lock by as the first arg
+class UDPLock < UDPSocket
+  attr_reader :port, :ip
 
-lock = UDPSocket.new
+  def initialize(port, ip='127.0.0.1')
+    super()
+    @port = port
+    @ip = ip
 
-begin
-  lock.bind('127.0.0.1',ARGV[0])
-  STDIN.gets
-rescue Errno::EADDRINUSE
-  puts "Lockport already in use on host"
-  exit 1
+    grab
+    close_read
+  end
+
+  # TODO(cuzelac): rescue more errors and more inteligent failure forms
+  def grab
+    begin
+      bind(@ip,@port)
+    rescue Errno::EADDRINUSE
+      raise Errno::EADDRINUSE, "lock already taken"
+    end
+  end
+
+  def clear
+    close
+  end
+
 end
+
+lck = UDPLock.new(12344)
+puts lck.inspect
+STDIN.gets
